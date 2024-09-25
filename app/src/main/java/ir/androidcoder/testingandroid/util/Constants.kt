c6e63a76999b3c59935ea4ab71a5abe11988c933
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-25 04:39:03 -1200

    Setup Pixabay Api Service

---
+++ app/src/main/java/ir/androidcoder/testingandroid/util/Constants.kt
@@ -0,0 +1,9 @@
+package ir.androidcoder.testingandroid.util
+
+object Constants {
+
+    const val API_KEY = "46180982-937dece49b4401cd5a0964362"
+    const val DATABASE_NAME = "shopping_db"
+    const val BASE_URL = "https://pixabay.com"
+
+}
