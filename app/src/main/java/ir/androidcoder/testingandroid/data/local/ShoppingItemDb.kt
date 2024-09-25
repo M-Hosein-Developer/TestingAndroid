ab03582011df07fd0a388603817570fcf8f03624
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-24 20:06:14 -1200

    Debugging Room Db Class

--- app/src/main/java/ir/androidcoder/testingandroid/data/local/ShoppingItemDb.kt
+++ app/src/main/java/ir/androidcoder/testingandroid/data/local/ShoppingItemDb.kt
@@ -1,9 +1,10 @@
 package ir.androidcoder.testingandroid.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [ShoppingItem::class], version = 1 , exportSchema = false)
-abstract class ShoppingItemDb {
+abstract class ShoppingItemDb : RoomDatabase(){
 
     abstract fun shoppingDao() : ShoppingDao
 
