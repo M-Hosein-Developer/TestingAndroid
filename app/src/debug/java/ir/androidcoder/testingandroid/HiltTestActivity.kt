cf45baf2890cd8bc14f040740f84a38ac3b891f2
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-10-06 04:22:18 -1200

    Create Hilt Test Activity

---
+++ app/src/debug/java/ir/androidcoder/testingandroid/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package ir.androidcoder.testingandroid
+
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
